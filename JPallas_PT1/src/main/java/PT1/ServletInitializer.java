455336735796ff124850e5bf0c886e8a5678f75b
Josep Mª Pallàs Batalla <jpb_montblanc@hotmail.com>
2023-04-19 10:14:10 +0200

    PT 1 First commit

---
+++ JPallas_PT1/src/main/java/PT1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package PT1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JPallasPt1Application.class);
+	}
+
+}
---
+++ JPallas_PT1/src/test/java/PT1/JPallasPt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package PT1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JPallasPt1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
