a411b1be2d9a21895d45f8d8ff4dbd6867d69cb9
Josep Mª Pallàs Batalla <jpb_montblanc@hotmail.com>
2023-04-19 12:38:38 +0200

    Hope this works D:

--- JPallas_PT1/src/main/java/PT1/service/PartyService.java
+++ JPallas_PT1/src/main/java/PT1/service/PartyService.java
@@ -3,9 +3,44 @@
  */
 package PT1.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import PT1.dao.PartyDAO;
+import PT1.dto.Party;
 
 @Service // Assign Service
 public class PartyService {
+
+	// Load DAO
+	@Autowired
+	PartyDAO partyDAO;
+
+	// CRUD functions
+	public List<Party> listParties() {
+		return partyDAO.findAll();
+	}
+
+	public List<Party> listPartiesByGameTitle(String game) {
+		return partyDAO.findPartiesByGameTitle(game);
+	}
+
+	public Party saveParty(Party party) {
+		return partyDAO.save(party);
+	}
+
+	public Party partyById(Long id) {
+		return partyDAO.findById(id).get();
+	}
+
+	public Party updateParty(Party party) {
+		return partyDAO.save(party);
+	}
+
+	public void deleteParty(Long id) {
+		partyDAO.deleteById(id);
+	}
 
 }
